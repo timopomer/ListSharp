411f5277575de4e26401e983b1f24557410873a2
timopomer <timopomer@gmail.com>
2015-11-12 22:10:33 +0200

    added more comments

--- ListSharp/Program.cs
+++ ListSharp/Program.cs
@@ -28,12 +28,12 @@ namespace ListSharp
             maincode.RemoveAll(string.IsNullOrWhiteSpace);
             List<string> alllists = new List<string>();
             string code = "public class MainClass" + Environment.NewLine + "{ " + Environment.NewLine + "public string Execute()" + Environment.NewLine + "{" + Environment.NewLine + "string temp_contents = \"\";" + Environment.NewLine + "string output = \"\";" + Environment.NewLine;
-            //creation of variables starts here
+            //variables initialization starts here:
             foreach (string singleline in maincode) {
 
 
 
-
+                //list variable
                 if (singleline.Substring(0,4) == "LIST")
                 {
                     _regex = new Regex(@"LIST([^>]*)=");
@@ -41,7 +41,7 @@ namespace ListSharp
                     alllists.Add("System.Collections.Generic.List<string> " + match.Groups[1].Value.Trim() + ";" + Environment.NewLine);
                 }
 
- 
+                //rows variable
                 if (singleline.Substring(0, 4) == "ROWS")
                 {
                     _regex = new Regex(@"ROWS([^>]*)=");
@@ -49,7 +49,7 @@ namespace ListSharp
                     alllists.Add("string[] " + match.Groups[1].Value.Trim() + ";" + Environment.NewLine);
                 }
 
-
+                //strg variable
                 if (singleline.Substring(0, 4) == "STRG")
                 {
                     _regex = new Regex(@"STRG([^>]*)=");
@@ -59,6 +59,10 @@ namespace ListSharp
 
             }
 
+            /*
+             * since it creates the appropiate code for each variable even if it occurs twice
+             * i am making sure there are no duplicate variable initalizations which would break the code
+             */
             alllists = alllists.Distinct().ToList<string>();
             foreach (string temp_string in alllists)
             {
@@ -68,12 +72,15 @@ namespace ListSharp
             int line = 0;
             foreach (string singleline in maincode)
             {
-                
-                line++;
-                string[] splitline = singleline.Split(new char[] { '=' }, 2);
+
+
+                line++; //to know what line number we are currently processing
+
+
+                string[] splitline = singleline.Split(new char[] { '=' }, 2); //splitting the line of "variable = evaluated string later to be parsed
                 splitline[1] = splitline[1].Substring(1);
 
-                if (splitline[0].Substring(0, 2) == "//")
+                if (splitline[0].Substring(0, 2) == "//") //to see if the code is commented out so it does net get into the final code (replaced with //skipped for debugging porpuses
                 {
                     code += "//skipped";
                     code += Environment.NewLine;
@@ -81,7 +88,7 @@ namespace ListSharp
                 }
 
 
-                if (splitline[0].Length < 4 || splitline[1].Length < 3)
+                if (splitline[0].Length < 4 || splitline[1].Length < 3) //checking that there is not too short/impossible line to break the interperter
                 {
                     Console.WriteLine("ListSharp exception: Line: " + line + " invalid");
                     while(true)
@@ -92,53 +99,54 @@ namespace ListSharp
                 }
 
 
-                string varname = splitline[0].Substring(4).Trim();
+                string varname = splitline[0].Substring(4).Trim(); //the first 4 characters will always be the variable type ex: strg,rows,list
 
-
+                //strg variable type
                 if (splitline[0].Substring(0, 4) == "STRG")
                 {
 
 
 
-                    if (splitline[1].Substring(0, 4) == "READ")
+                    if (splitline[1].Substring(0, 4) == "READ") //read text file into code command is called "read"
                     {
-                        _regex = new Regex(@"READ\[([^>]*)\]");
+                        _regex = new Regex(@"READ\[([^>]*)\]"); //everything between the square brackets "[path]"
                         match = _regex.Match(splitline[1]);
                         string path = @match.Groups[1].Value.Trim();
 
-                        if (!File.Exists(path))
+                        if (!File.Exists(path)) //checking that the file is readable
                         {
                             Console.WriteLine("ListSharp exception: File does not exist, aborting operation\nadditional information: File:\"" + path + "\"");
 
                             while (true)
                             {
-                                Thread.Sleep(1000);
+                                Thread.Sleep(1000); //sleep forever
                             }
                         }
-                        code += "temp_contents = System.IO.File.ReadAllText(@\"" + path + "\");";
+                        code += "temp_contents = System.IO.File.ReadAllText(@\"" + path + "\");"; //create the reading file code in interperted form that is read into a tempoary variable
                         code += Environment.NewLine;
 
                     }
 
-                    code += varname + " = temp_contents;";
+                    code += varname + " = temp_contents;"; //set variable to tempoary variable
 
                 }
 
+                //rows variable type
                 if (splitline[0].Substring(0, 4) == "ROWS")
                 {
-                    if (splitline[1].Substring(0, 8) == "ROWSPLIT")
+                    if (splitline[1].Substring(0, 8) == "ROWSPLIT") //rowsplit command
                     {
-                        _regex = new Regex(@"ROWSPLIT([^>]*)BY");
+                        _regex = new Regex(@"ROWSPLIT([^>]*)BY"); //this finds what variable is to be split
                         match = _regex.Match(splitline[1]);
                         string invar = match.Groups[1].Value.Trim();
 
-                        _regex = new Regex(@"\[(.*)\]");
+                        _regex = new Regex(@"\[(.*)\]"); //this finds by what string to split the variable
                         match = _regex.Match(splitline[1]);
                         string bywhat = match.Groups[1].Value.Trim();
 
-                        bywhat = bywhat.Replace("<newline>", "System.Environment.NewLine");
+                        bywhat = bywhat.Replace("<newline>", "System.Environment.NewLine"); //so you can split by newline by saying the string is <newline>
 
-                        code += varname + " = " + invar + ".Split(new string[] { " + bywhat + " }, System.StringSplitOptions.None);"; 
+                        code += varname + " = " + invar + ".Split(new string[] { " + bywhat + " }, System.StringSplitOptions.None);"; //interperted code
                     }
                 }
 
@@ -149,11 +157,12 @@ namespace ListSharp
                     //code += match.Groups[1].Value.Trim() + " = ";   
                 }
 
+                //show a variable to debug your program
                 if (splitline[0].Substring(0, 4) == "SHOW")
                 {
                         _regex = new Regex(@"([^>]*);");
                         match = _regex.Match(splitline[1]);
-                        code += "output = makeOutput(" + match.Groups[1].Value.Trim() + " , output);";
+                        code += "output = makeOutput(" + match.Groups[1].Value.Trim() + " , output);"; //call makeOutput() on any type of variable the users wants to display
                         code += Environment.NewLine;
                 }
                 code += Environment.NewLine;
@@ -277,7 +286,10 @@ output += System.Environment.NewLine + "---------------output--------------" + S
                  
                  Console.WriteLine(output);
             }
-            Thread.Sleep(1000000);
+            while (true)
+            {
+                Thread.Sleep(1000); //sleep forever
+            }
         }
     }
 }
